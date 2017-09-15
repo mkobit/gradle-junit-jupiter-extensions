366dc3038761036d3370d18498afc12b91b94efe
Mike Kobit <mkobit@gmail.com>
2017-09-15 14:19:15 -0500

    Add some more method to AssertJ extensions
    The tests need to be refactored to be clearer about what they are actually testing and could also be missing some useful methods on the assertion provider.

--- assertj-extensions/src/main/java/com/mkobit/gradle/testkit/assertj/BuildResultAssert.java
+++ assertj-extensions/src/main/java/com/mkobit/gradle/testkit/assertj/BuildResultAssert.java
@@ -3,10 +3,15 @@ package com.mkobit.gradle.testkit.assertj;
 import org.assertj.core.api.AbstractAssert;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.BuildTask;
+import org.gradle.testkit.runner.TaskOutcome;
 
 import javax.annotation.Nullable;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Objects;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 public class BuildResultAssert extends AbstractAssert<BuildResultAssert, BuildResult> {
   public BuildResultAssert(final @Nullable BuildResult actual) {
@@ -14,7 +19,6 @@ public class BuildResultAssert extends AbstractAssert<BuildResultAssert, BuildRe
   }
 
   //  String getOutput();
-  // TODO: look at assertJ feature for breaking method change for contains() for reasoning why it is being change
   public BuildResultAssert outputContains(final CharSequence sequence) {
     isNotNull();
 
@@ -65,11 +69,87 @@ public class BuildResultAssert extends AbstractAssert<BuildResultAssert, BuildRe
     return this;
   }
 
-  public BuildResultAssert hasTaskAtPathSatisfying(final CharSequence path, final Consumer<BuildTask> requirements) {
+  public BuildResultAssert hasTaskAtPathSatisfying(final CharSequence path, final Consumer<? super BuildTask> requirements) {
+    isNotNull();
     Objects.requireNonNull(requirements, "Consumer<BuildTask> of task expressing assertions requirements must not be null");
     final String taskPath = path.toString();
     final BuildTask buildTask = actual.task(taskPath);
     requirements.accept(buildTask);
     return this;
+  }
+
+  public BuildResultAssert tasksWithOutcomeSatisfy(
+    final TaskOutcome outcome,
+    final Consumer<List<BuildTask>> requirements
+  ) {
+    isNotNull();
+    Objects.requireNonNull(outcome, "outcome must not be null");
+    Objects.requireNonNull(requirements, "requirements must not be null");
+    requirements.accept(actual.tasks(outcome));
+    return this;
+  }
+
+  public BuildResultAssert taskPathsWithOutcomeSatisfy(
+    final TaskOutcome outcome,
+    final Consumer<List<String>> requirements
+  ) {
+    isNotNull();
+    Objects.requireNonNull(outcome, "outcome must not be null");
+    Objects.requireNonNull(requirements, "requirements must not be null");
+    requirements.accept(actual.taskPaths(outcome));
+    return this;
+  }
+
+  public BuildResultAssert tasksSatisfy(final Consumer<List<BuildTask>> requirements) {
+    isNotNull();
+    Objects.requireNonNull(requirements, "requirements must not be null");
+    requirements.accept(actual.getTasks());
+    return this;
+  }
+
+  public BuildResultAssert hasTaskFailedAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.FAILED);
+  }
+
+  public BuildResultAssert hasTaskFromCacheAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.FROM_CACHE);
+  }
+
+  public BuildResultAssert hasTaskNoSourceAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.NO_SOURCE);
+  }
+
+  public BuildResultAssert hasTaskSkippedAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.SKIPPED);
+  }
+
+  public BuildResultAssert hasTaskSuccessAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.SUCCESS);
+  }
+
+  public BuildResultAssert hasTaskUpToDateAtPath(final CharSequence path) {
+    return hasTaskAtPathWithOutcome(path, TaskOutcome.UP_TO_DATE);
+  }
+
+  public BuildResultAssert hasTaskAtPathWithOutcome(final CharSequence path, final TaskOutcome outcome) {
+    isNotNull();
+    Objects.requireNonNull(outcome, "outcome must not be null");
+    final String taskPath = path.toString();
+    final BuildTask buildTask = actual.task(taskPath);
+    if (buildTask == null) {
+      failWithMessage(
+        "Build task at path %s was not executed so could not be checked for outcome %s.%nTasks executed by build were:%n %s", taskPath, outcome, formatTasksForFailureMessage(actual.getTasks()));
+    }
+    if (buildTask.getOutcome() != outcome) {
+      failWithMessage("Build task at path %s expected to have outcome:%n <%s>%nbut was:%n <%s>", taskPath, outcome, buildTask.getOutcome());
+    }
+    return this;
+  }
+
+  private String formatTasksForFailureMessage(final List<BuildTask> tasks) {
+    return tasks.stream()
+                .sorted(Comparator.comparing(BuildTask::getPath))
+                .map(buildTask -> "(path=" + buildTask.getPath() + ", " + "outcome=" + buildTask.getOutcome())
+                .collect(Collectors.joining(", ", "[", "]"));
   }
 }
--- assertj-extensions/src/test/kotlin/com/mkobit/gradle/testkit/assertj/BuildResultAssertTest.kt
+++ assertj-extensions/src/test/kotlin/com/mkobit/gradle/testkit/assertj/BuildResultAssertTest.kt
@@ -1,35 +1,43 @@
 package com.mkobit.gradle.testkit.assertj
 
-import com.nhaarman.mockito_kotlin.atLeastOnce
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.nhaarman.mockito_kotlin.times
 import com.nhaarman.mockito_kotlin.verify
-import org.assertj.core.api.Assertions.assertThat
+import com.nhaarman.mockito_kotlin.whenever
 import org.assertj.core.api.Assertions.assertThatCode
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.BuildTask
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.verification.VerificationMode
 import java.util.function.Consumer
 
 internal class BuildResultAssertTest {
 
+  private lateinit var mockBuildResult: BuildResult
+  private lateinit var mockBuildTask: BuildTask
+  private lateinit var buildResultAssert: BuildResultAssert
+
+  @BeforeEach
+  internal fun setUp() {
+    mockBuildResult = mock()
+    mockBuildTask = mock()
+    buildResultAssert = BuildResultAssert(mockBuildResult)
+  }
+
   @Test
   internal fun `constructed with null instance`() {
-    val buildResultAssert = BuildResultAssert(null)
-    assertThatThrownBy { buildResultAssert.isNotNull }
-    assertThatCode { buildResultAssert.isNull() }.doesNotThrowAnyException()
+    val nullActualBuildResultAssert = BuildResultAssert(null)
+    assertThatThrownBy { nullActualBuildResultAssert.isNotNull }.isInstanceOf(AssertionError::class.java)
+    assertThatCode { nullActualBuildResultAssert.isNull() }.doesNotThrowAnyException()
   }
 
   @Test
   internal fun `output contains`() {
     val buildOutput = "this is the build output"
-    val mockBuildResult: BuildResult = mock {
-      on { output } doReturn buildOutput
-    }
-    val buildResultAssert = BuildResultAssert(mockBuildResult)
+    whenever(mockBuildResult.output).thenReturn(buildOutput)
 
     assertThatThrownBy { buildResultAssert.outputContains("nope present") }.isInstanceOf(AssertionError::class.java)
     assertThatCode { buildResultAssert.outputContains("build output") }.doesNotThrowAnyException()
@@ -38,10 +46,7 @@ internal class BuildResultAssertTest {
   @Test
   internal fun `output does not contain`() {
     val buildOutput = "this is the build output"
-    val mockBuildResult: BuildResult = mock {
-      on { output } doReturn buildOutput
-    }
-    val buildResultAssert = BuildResultAssert(mockBuildResult)
+    whenever(mockBuildResult.output).thenReturn(buildOutput)
 
     assertThatCode { buildResultAssert.outputDoesNotContain("nope present") }.doesNotThrowAnyException()
     assertThatThrownBy { buildResultAssert.outputDoesNotContain("build output") }.isInstanceOf(AssertionError::class.java)
@@ -50,15 +55,157 @@ internal class BuildResultAssertTest {
   @Test
   internal fun `output satisfies`() {
     val buildOutput = "this is the build output"
-    val mockBuildResult: BuildResult = mock {
-      on { output } doReturn buildOutput
-    }
+    whenever(mockBuildResult.output).thenReturn(buildOutput)
     val mockConsumer = mock<Consumer<String>>()
-    val buildResultAssert = BuildResultAssert(mockBuildResult)
 
     assertThatCode { buildResultAssert.outputSatisfies(mockConsumer) }.doesNotThrowAnyException()
 
     verify(mockConsumer, times(1)).accept(buildOutput)
+  }
+
+  @Test
+  internal fun `tasks with outcome satisfy`() {
+    val buildTasks = listOf(mockBuildTask)
+    val taskOutcome = TaskOutcome.SUCCESS
+    whenever(mockBuildResult.tasks(taskOutcome)).thenReturn(buildTasks)
+    val mockTasksConsumer: Consumer<List<BuildTask>> = mock()
+
+    assertThatCode {
+      buildResultAssert.tasksWithOutcomeSatisfy(taskOutcome, mockTasksConsumer)
+    }.doesNotThrowAnyException()
+    verify(mockBuildResult, times(1)).tasks(taskOutcome)
+    verify(mockTasksConsumer, times(1)).accept(buildTasks)
+  }
+
+  @Test
+  internal fun `task paths with outcome satisfy`() {
+    val taskPaths = listOf(":path1")
+    val taskOutcome = TaskOutcome.SUCCESS
+    whenever(mockBuildResult.taskPaths(taskOutcome)).thenReturn(taskPaths)
+    val mockTaskPathsConsumer: Consumer<List<String>> = mock()
+
+    assertThatCode {
+      buildResultAssert.taskPathsWithOutcomeSatisfy(taskOutcome, mockTaskPathsConsumer)
+    }.doesNotThrowAnyException()
+    verify(mockBuildResult, times(1)).taskPaths(taskOutcome)
+    verify(mockTaskPathsConsumer, times(1)).accept(taskPaths)
+  }
+
+  @Test
+  internal fun `tasks satisfy`() {
+    val tasks = listOf(mockBuildTask)
+    whenever(mockBuildResult.tasks).thenReturn(tasks)
+    val mockTasksConsumer: Consumer<List<BuildTask>> = mock()
+
+    assertThatCode {
+      buildResultAssert.tasksSatisfy(mockTasksConsumer)
+    }.doesNotThrowAnyException()
+    verify(mockBuildResult, times(1)).tasks
+    verify(mockTasksConsumer, times(1)).accept(tasks)
+  }
+
+  @Test
+  internal fun `has task at path with failed task outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.FAILED)
+    assertThatCode {
+      buildResultAssert.hasTaskFailedAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskFailedAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with from cache outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.FROM_CACHE)
+    assertThatCode {
+      buildResultAssert.hasTaskFromCacheAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskFromCacheAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with no source outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.NO_SOURCE)
+    assertThatCode {
+      buildResultAssert.hasTaskNoSourceAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskNoSourceAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with skipped outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SKIPPED)
+    assertThatCode {
+      buildResultAssert.hasTaskSkippedAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskSkippedAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with success outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatCode {
+      buildResultAssert.hasTaskSuccessAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.FAILED)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskSuccessAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with up-to-date outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.UP_TO_DATE)
+    assertThatCode {
+      buildResultAssert.hasTaskUpToDateAtPath(taskPath)
+    }.doesNotThrowAnyException()
+
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.SUCCESS)
+    assertThatThrownBy {
+      buildResultAssert.hasTaskUpToDateAtPath(taskPath)
+    }.isInstanceOf(AssertionError::class.java)
+  }
+
+  @Test
+  internal fun `has task at path with user - specified outcome`() {
+    val taskPath = ":taskPath"
+    whenever(mockBuildResult.task(taskPath)).thenReturn(mockBuildTask)
+    whenever(mockBuildTask.outcome).thenReturn(TaskOutcome.FAILED)
+    assertThatCode {
+      buildResultAssert.hasTaskAtPathWithOutcome(taskPath, TaskOutcome.FAILED)
+    }.doesNotThrowAnyException()
+
+    assertThatThrownBy {
+      buildResultAssert.hasTaskAtPathWithOutcome(taskPath, TaskOutcome.SUCCESS)
+    }.isInstanceOf(AssertionError::class.java)
   }
 
   @Test
@@ -78,10 +225,7 @@ internal class BuildResultAssertTest {
   @Test
   internal fun `does not have task at path`() {
     val path = ":taskPath"
-    val mockBuildTask: BuildTask = mock()
-    val mockBuildResult: BuildResult = mock {
-      on { task(path) } doReturn mockBuildTask
-    }
+    whenever(mockBuildResult.task(path)).thenReturn(mockBuildTask)
     val buildResultAssert = BuildResultAssert(mockBuildResult)
 
     assertThatThrownBy { buildResultAssert.doesNotHaveTaskAtPath(":taskPath") }.isInstanceOf(AssertionError::class.java)
@@ -92,10 +236,7 @@ internal class BuildResultAssertTest {
   @Test
   internal fun `task at path satisfies`() {
     val path = ":taskPath"
-    val mockBuildTask: BuildTask = mock()
-    val mockBuildResult: BuildResult = mock {
-      on { task(path) } doReturn mockBuildTask
-    }
+    whenever(mockBuildResult.task(path)).thenReturn(mockBuildTask)
     val mockBuildTaskConsumer: Consumer<BuildTask?> = mock()
     val buildResultAssert = BuildResultAssert(mockBuildResult)
 
